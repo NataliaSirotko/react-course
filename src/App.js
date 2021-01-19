1e98306a5bd29dc026460082ca7726272e21d5cd
Natalia Sirotko <NSirotka@gomel.iba.by>
2021-01-19 21:00:56 +0300

    Understanding the Basic. Task3.
    Adding Two Way Binding.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Header from './Header';
 
 function App() {
   const [ cardState, setCardState ] = useState({
-      caption: 'Caption', text: 'Text...', checked: false
+      caption: 'Caption', text: 'Text...', checked: false,
+      edited: false
   });
 
   function checkboxChange(event) {
@@ -15,10 +16,40 @@ function App() {
     });
   }
 
+  function editCard() {
+    setCardState({
+      ...cardState,
+      edited: !cardState.edited,
+      checked: (cardState.checked ? !cardState.checked : cardState.checked)
+    });
+  }
+
+  function captionChange(event) {
+    setCardState({
+      ...cardState,
+      caption: event.target.value,
+    });
+  }
+
+  function textChange(event) {
+    setCardState({
+      ...cardState,
+      text: event.target.value
+    });
+  }
+
+  function cancelEditing() {
+    setCardState({
+      caption: 'Caption', text: 'Text...',
+      edited: !cardState.edited
+    });
+  }
+
   return (
     <div className="App">
       <Header></Header>
-      <Card className="block" caption={cardState.caption} text={cardState.text} checked={cardState.checked} checkboxChange={checkboxChange}></Card>
+      <Card className="block" caption={cardState.caption} text={cardState.text} checked={cardState.checked} checkboxChange={checkboxChange}
+            edit={editCard} edited={cardState.edited} capEdit={captionChange} textEdit={textChange} cancel={cancelEditing}></Card>
       <Card className="block" caption="Cap" text="Text..."></Card>
     </div>
   );
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import './index.css';
 import classNames from 'classnames';
+import { IoIosSave } from 'react-icons/io';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { MdCancel } from 'react-icons/md';
 
-//var classNames = require('classnames');
-const Card = ({caption, text, checked, checkboxChange}) => {
+const Card = ({caption, text, checked, checkboxChange, edited, edit, capEdit, textEdit, cancel}) => {
     let cardClass = classNames('card', {
         'card-checked': checked
     });
+    let elemsClass = classNames({
+       'active': !edited,
+       'non-active': edited
+    });
+    let elemsEditedClass = classNames({
+        'active': edited,
+        'non-active': !edited
+     });
 
     return <div className={cardClass}>
-        <p>{caption}
-            <input type="checkbox" checked={checked} onChange={checkboxChange}></input>
+        <p><span className={elemsClass}>{caption}</span>
+            <input className={elemsEditedClass} type="text" onChange={capEdit} value={caption} />
+            <span className="iconBox">
+                <span className={elemsClass} onClick={edit}><AiOutlineEdit /></span>
+                <input className={elemsClass} type="checkbox" checked={checked} onChange={checkboxChange} />
+                <span className={elemsEditedClass} onClick={edit}><IoIosSave /></span>
+                <span className={elemsEditedClass} onClick={cancel}><MdCancel /></span>
+            </span>
         </p>
-        <p>{text}</p>
+        <p className={elemsClass}>{text}</p>
+        <textarea className={elemsEditedClass} onChange={textEdit} value={text}></textarea>
     </div>
 };
 
