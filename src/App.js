42b152841214b743123b21dcceb94b49b76d0199
Natalia Sirotko <NSirotka@gomel.iba.by>
2021-01-31 21:31:21 +0300

    Styling React Components. Task5.
    Adding Styled Components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React, { useState } from 'react';
 import './App.css';
 import Card from './Card';
 import Header from './Header';
+import styled from 'styled-components';
+
+const Checkbox = styled.input`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border: 2px solid violet;
+  border-radius: 5px;
+  margin: 0 10px;
+  vertical-align: top;
+  outline: none;
+  transition: all 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    border-width: 4px;
+  }
+
+  &:active {
+    background: violet;
+  }
+
+  &:checked {
+    height: .6rem;
+    border-width: 4px;
+    border-top-style: none;
+    border-right-style: none;
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -ms-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+  }
+  `;
 
 function App() {
   const [ cardState, setCardState ] = useState({
@@ -145,7 +181,7 @@ function App() {
       <Header></Header>
       <main>
         <label>Только просмотр
-          <input type="checkbox" checked={cardState.checked} onChange={mainChecked}  />
+          <Checkbox type="checkbox" checked={cardState.checked} onChange={mainChecked}  />
         </label>
         <div className="cardblock">
           {cardState.cards.map(card => {
